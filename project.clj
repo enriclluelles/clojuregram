b941c01f6c18af2e86bae28f55ddc96753291164
Enric Lluelles <enric@lluell.es>
2014-12-19 10:51:14 +0100

    More complete project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject clojuregram "0.1.0-SNAPSHOT"
+(defproject clojuregram "0.1.1"
   :description "Simple Clojure instagram API wrapper"
-  :url "http://github.com/enriclluelles/clojuregram"
+  :url "https://github.com/enriclluelles/clojuregram"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "1.0.1"]])
+                 [clj-http "1.0.1"]]
+  :scm {:name "git"
+        :url "https://github.com/enriclluelles/clojuregram"})
